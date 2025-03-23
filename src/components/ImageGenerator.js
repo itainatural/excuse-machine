a66b597cd2b58b8591fc6fa22ea2c27a8568485f
itainatural <itai.agami@naturalint.com>
2025-03-23 12:03:22 +0200

    style: improve UI to match reference design

--- src/components/ImageGenerator.js
+++ src/components/ImageGenerator.js
@@ -21,7 +21,7 @@ const imageMoods = {
 };
 
 const MicrophoneIcon = ({ isRecording }) => (
-  <svg width="24" height="24" viewBox="0 0 24 24" fill="none" stroke={isRecording ? "#FF3B30" : "currentColor"} strokeWidth="2">
+  <svg width="24" height="24" viewBox="0 0 24 24" fill="none" stroke={isRecording ? "#34C759" : "currentColor"} strokeWidth="2">
     <path d="M12 1a3 3 0 0 0-3 3v8a3 3 0 0 0 6 0V4a3 3 0 0 0-3-3z" />
     <path d="M19 10v2a7 7 0 0 1-14 0v-2M12 19v4M8 23h8" />
   </svg>
