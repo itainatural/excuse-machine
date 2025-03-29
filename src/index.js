384aaac4156b8b67556e1f39e49577f22d58ad5d
itainatural <itai.agami@naturalint.com>
2025-03-29 22:37:29 +0300

    Fix service worker issues and clean up index.js

--- src/index.js
+++ src/index.js
@@ -11,17 +11,7 @@ root.render(
   </React.StrictMode>
 );
 
-// Register service worker
-if ('serviceWorker' in navigator && process.env.NODE_ENV === 'production') {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/serviceWorker.js')
-      .then(registration => {
-        console.log('SW registered:', registration);
-      })
-      .catch(error => {
-        console.log('SW registration failed:', error);
-      });
-  });
-}
-
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
